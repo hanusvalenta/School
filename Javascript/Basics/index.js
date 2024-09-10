2dde6ed02f4d35726dfac43f77a192c972dae09f
Hanuš Valenta <93769207+hanusvalenta@users.noreply.github.com>
2024-09-10 10:41:27 +0200

    guessing game

--- Javascript/Basics/index.js
+++ Javascript/Basics/index.js
@@ -1,1 +1,10 @@
-alert("huh")
+function myFunction() {
+    var x = document.getElementById("myNumber").value;
+    let y = Math.floor(Math.random() * 10);
+    if(x == y){
+        document.getElementById("demo").innerHTML = "hadal jsi spravne";
+    }
+    else{
+        document.getElementById("demo").innerHTML = "hadal jsi spatne";
+    }
+  }
