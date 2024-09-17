c0b8e938b833f19ba7e830c01ceb1d354c4a1fac
Hanuš Valenta <93769207+hanusvalenta@users.noreply.github.com>
2024-09-17 09:35:09 +0200

    make it better and stronger

--- Javascript/Basics/index.js
+++ Javascript/Basics/index.js
@@ -2,9 +2,9 @@ function myFunction() {
     var x = document.getElementById("myNumber").value;
     let y = Math.floor(Math.random() * 10);
     if(x == y){
-        document.getElementById("demo").innerHTML = "hadal jsi spravne";
+        document.getElementById("demo").innerHTML = "Uhodl jsi :)";
     }
     else{
-        document.getElementById("demo").innerHTML = "hadal jsi spatne";
+        document.getElementById("demo").innerHTML = "Hádal jsi špatně číslo na uhodnutí bylo " + y;
     }
   }
