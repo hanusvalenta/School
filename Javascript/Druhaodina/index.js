86c4833a85753d237c0ed06ba3c4501c596ae9f6
Hanuš Valenta <93769207+hanusvalenta@users.noreply.github.com>
2024-09-24 11:00:10 +0200

    ehhh

--- Javascript/Druhaodina/index.js
+++ Javascript/Druhaodina/index.js
@@ -1,38 +1,45 @@
 let studenti = [];
 
-function zapsatSe() {
+function zapsatSe(){
     let fname = document.getElementById('fname').value;
     let fLastName = document.getElementById('fLastName').value;
+    let fVek = document.getElementById('fVek').value;
     let fKurz = document.getElementById('fKurz').value;
 
     let student = {
         jmeno: fname,
         prijmeni: fLastName,
+        vek: fVek,
         kurz: fKurz
     };
 
-    if(fname != '' & fLastName != '' & fKurz != ''){
+    if(fname != '' & fLastName != '' & fKurz != '' & fVek != ''){
         studenti.push(student);
 
-        document.getElementById('zapsano').innerHTML = 'Student zapsán!';
+        document.getElementById('text').innerHTML = 'Student zapsán!';
         console.log("Student zapsán!")
     }
     else{
-        document.getElementById('zapsano').innerHTML = 'Neplatné údaje!';
+        document.getElementById('text').innerHTML = 'Neplatné údaje!';
         console.log("Neplatné údaje!")
     }
     
  
     document.getElementById('fname').value = '';
     document.getElementById('fLastName').value = '';
+    document.getElementById('fVek').value = '';
     document.getElementById('fKurz').value = '';
 }
 
-function ukazat() {
+function filterByAge(){
+
+}
+
+function ukazat(){
     let output = '';
     studenti.forEach((student, index) => {
-        output += (index + 1) + '. ' + student.jmeno + ' ' + student.prijmeni + ' - ' + student.kurz + '<br>';
+        output += student.jmeno + ' ' + student.prijmeni + ' ' + student.fVek + ' - ' + student.kurz + '<br>';
     });
 
-    document.getElementById('ukazka').innerHTML = output;
+    document.getElementById('text').innerHTML = output;
 }
