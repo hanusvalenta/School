8be0138889efab424c5ad2b6276999c107542308
Valenta Hanus <hanus.valenta@gmail.com>
2024-11-07 09:47:45 +0100

    biggest num

--- Java/pole/src/Main.java
+++ Java/pole/src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int pole[] = {4,14,5,9,9,40};
-        int soucet = 0;
-        int sudaCisla = 0;
+        int pole[] = {4,14,5,1,9,40};
+        int nejVetsi = pole[0];
+        int nejMensi = pole[0];
 
         for (int i = 0; i < pole.length; i++) {
-            if (pole[i] % 2 == 0) {
-                soucet += pole[i];
-                sudaCisla++;
-            } 
+            if (pole[i] > nejVetsi) {
+                nejVetsi = pole[i];
+            }
+            if (pole[i] < nejMensi) {
+                nejMensi = pole[i];
+            }
         }
 
-        int prumer = soucet/sudaCisla;
-
-        System.out.println(prumer);
+        System.out.println(nejVetsi + " " + nejMensi);
     }
 }
