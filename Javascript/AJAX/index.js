a8465beb0b834bd9873b59d97ee58442544af9d7
Valenta Hanus <hanus.valenta@gmail.com>
2024-11-12 08:59:41 +0100

    write out

--- Javascript/AJAX/index.js
+++ Javascript/AJAX/index.js
@@ -2,6 +2,6 @@ fetch('data.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        document.getElementById('text').innerHTML = data;
+        document.getElementById('text').innerHTML = JSON.stringify(data, null, 2);
     })
     .catch(error => console.error('Error fetching data:', error));
