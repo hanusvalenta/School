f2d8f0769851a97a30ccbecb6a74e0019fdc2d9d
Valenta Hanus <hanus.valenta@gmail.com>
2025-01-16 08:20:59 +0100

    new test start

---
+++ Java/testnemec2/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int a = 10;
+        switch(a){
+            case 10:
+                System.out.println("10");
+                break;
+            case 11:
+                System.out.println("11");
+                break;
+        }
+
+        while (a > 10){
+            System.out.println(a);
+        }
+    }
+}
