34bbe3528cea6be95a6a9339efd5183bc1f00bee
Valenta Hanus <hanus.valenta@gmail.com>
2025-01-28 21:42:15 +0100

    test n2mec 3

--- Java/NemecTest3/src/IsOdd.java
+++ Java/NemecTest3/src/IsOdd.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class IsOdd {
+    Scanner scanner = new Scanner(System.in);
+    boolean isOdd() {
+        return scanner.nextInt() % 2 == 1;
+    }
 }
---
+++ Java/NemecTest3/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        IsOdd IsOdd = new IsOdd();
+        if (IsOdd.isOdd()) {System.out.println("Tvoje číslo je liché");}
+        else {System.out.println("Tvoje číslo je sudé");}
+    }
+}
