1d67252f747bfc674543fbb18f9a9e29923ed1ed
Hanus Valenta <hanus.valenta@gmail.com>
2025-03-11 09:02:27 +0100

    cool it works

--- Javascript/UkazkaHBS/public/app.js
+++ Javascript/UkazkaHBS/public/app.js
@@ -20,6 +20,10 @@ hbs.registerPartial('partial_name', 'partial value');
 
 app.set('views', path.join(__dirname, 'views'));
 
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
