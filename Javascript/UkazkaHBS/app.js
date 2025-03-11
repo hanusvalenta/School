9d471f7865c4d1febd1260ebf91f616278c1747c
Hanus Valenta <hanus.valenta@gmail.com>
2025-03-11 08:56:21 +0100

    hbs setup

---
+++ Javascript/UkazkaHBS/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const hbs = require('hbs');
+const path = require('path');
+
+const app = express();
+
+app.set('view engine', 'hbs');
+
+hbs.registerPartials(path.join(__dirname, 'views/partials'), function (err) {
+    if (err) {
+        console.error('Error registering partials:', err);
+    }
+});
+
+hbs.registerHelper('helper_name', function (options) {
+    return 'helper value';
+});
+
+hbs.registerPartial('partial_name', 'partial value');
+
+app.set('views', path.join(__dirname, 'views'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
