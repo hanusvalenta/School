d54a76203d7ba233814f692142fe78c4233d8cd0
Hanus Valenta <hanus.valenta@gmail.com>
2025-03-27 10:49:33 +0100

    changes

---
+++ Java/HodinaNemec/src/Student.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Student {
+    private int id = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
