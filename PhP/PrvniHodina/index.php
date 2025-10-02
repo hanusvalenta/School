d3cb4331008593762753231fb1e02777f586c1ee
Hanus Valenta <hanus.valenta@gmail.com>
2025-10-02 15:19:42 +0200

    workikng

--- PhP/PrvniHodina/index.php
+++ PhP/PrvniHodina/index.php
@@ -2,15 +2,42 @@
 <html>
 <head>
     <title>Test</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Test</h1>
+
+    <form action="index.php" method="get">
+        <input type="text" name="text">
+        <input type="password" name="heslo">
+
+        <br>
+        <input type="checkbox" name="check" value="check" checked>
+
+        <input type="radio" name="radio" value="on" checked>
+        <input type="radio" name="radio" value="off">
+
+        <input type="file" name="file">
+
+        <select name="select" size="5" multiple>
+            <option value="lol">lol</option>
+            <option value="nah">lol</option>
+        </select>
+
+        <input type="submit" name="submit">
+    </form>
+
+    <?php
+    if(isset($_GET['submit'])){
+        $radio = $_GET['radio'];
+        if ($radio == 'on') {
+            echo "😀";
+        }
+        else if ($radio == 'off') {
+            echo "🤨";
+        }
+    }
+    ?>
 </body>
 </html>
 
-<?php
-$text = 'nejaki debil mi odrel auto';
-
-$newtxt = str_replace("debil", '😁' , $text);
-echo str_replace("odrel", '😭' , $newtxt);
-?>
