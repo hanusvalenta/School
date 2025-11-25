663cde51f0a98478314300a97cf5db9a93841830
Hanus Valenta <hanus.valenta@gmail.com>
2025-11-25 08:37:40 +0100

    FileRLE start

---
+++ CPP/FilesRLE/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    std::cout << R"(
+     ________  __  __           
+/        |/  |/  |          
+$$$$$$$$/ $$/ $$ |  ______  
+$$ |__    /  |$$ | /      \ 
+$$    |   $$ |$$ |/$$$$$$  |
+$$$$$/    $$ |$$ |$$    $$ |
+$$ |      $$ |$$ |$$$$$$$$/ 
+$$ |      $$ |$$ |$$       |
+$$/       $$/ $$/  $$$$$$$/ 
+                            
+                            
+                            
+)" << '\n';
+
+    ofstream outFile("text.bussy");
+
+    outFile << "je realny";
+
+    outFile.close();
+
+    string fileContent;
+
+    ifstream inFile("text.bussy");
+
+    getline(inFile, fileContent);
+
+    cout << "Novy obsah souboru - " << fileContent << endl;
+
+    inFile.close();
+
+    return 0;
+}
