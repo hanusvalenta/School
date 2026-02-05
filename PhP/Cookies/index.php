ab646cbe825fc5f45b09668018882e8c9f5c1269
Hanus Valenta <hanus.valenta@gmail.com>
2026-02-05 15:10:57 +0100

    chockolate starfish

---
+++ PhP/Cookies/index.php
@@ -0,0 +1,29 @@
+<?php
+    function Redirect($url, $permanent = false)
+    {
+        header('Location: ' . $url, true, $permanent ? 301 : 302);
+        exit();
+    }
+
+    //require_once ("Db.php");
+    //Db::connect('127.0.0.1', 'test', 'root', '');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Formulář</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+        if (isset($_COOKIE["onEnter"])){
+            setcookie("onEnter", $_COOKIE["onEnter"] + 1, time() + 5);
+            echo $_COOKIE["onEnter"];
+        }
+        else {
+            echo "no nookie";
+            setcookie("onEnter", 1, time() + 5);
+        }
+    ?>
+</body>
+</html>
